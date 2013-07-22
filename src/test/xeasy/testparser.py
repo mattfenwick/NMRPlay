0b7b4751f0885cc9dccf3dbf012b9f1212a68439
Matt Fenwick <mfenwick100@gmail.com>
2013-07-22 17:27:18 -0400

    remove unit tests that belong in a different project; fix up xeasy parser unit tests

--- src/test/xeasy/testparser.py
+++ src/test/xeasy/testparser.py
@@ -4,9 +4,8 @@ Created on Apr 29, 2013
 @author: mattf
 '''
 import xeasy.parser as p
-import parse.position as np
-import parse.maybeerror as me
-import parse.conslist as c
+import unparse.maybeerror as me
+import unparse.conslist as c
 import unittest as u
 import xeasy.model as mod
 
@@ -18,32 +17,28 @@ def good(rest, state, result):
     return m.pure({'rest': rest, 'state': state, 'result': result})
 
 def runParser(parser, inp):
-    return parser.parse(l(inp), None)
+    return parser.parse(l(inp), (1, 1))
 
 
 class TestXEasy(u.TestCase):
     
     def testLine1(self):
         inp = '# Number of dimensions 2\nhello'
-        inp1 = np.addLineCol(inp)
-        self.assertEqual(good(l(inp1[-5:]), None, 2), runParser(p.line1, inp1))
+        self.assertEqual(good(l(inp[-5:]), (2,1), 2), runParser(p.line1, inp))
     
     def testDims(self):
         inp = '# INAME 1 h\n# INAME 2 N\noops'
-        inp1 = np.addLineCol(inp)
-        self.assertEqual(good(l(inp1[-4:]), None, ['h', 'N']), runParser(p.dims(2), inp1))
-        self.assertEqual(me.MaybeError.zero, runParser(p.dims(2), inp1[:20]))
+        self.assertEqual(good(l(inp[-4:]), (3,1), ['h', 'N']), runParser(p.dims(2), inp))
+        self.assertEqual(me.MaybeError.zero, runParser(p.dims(2), inp[:20]))
     
     def testPeakLine(self):
         inp = ' 12 1.2 3.4 a b 1123\noops'
-        inp1 = np.addLineCol(inp)
-        self.assertEqual(good(l(inp1[-4:]), None, (12, mod.Peak([1.2, 3.4], 1123))), runParser(p.peakline(2), inp1))
+        self.assertEqual(good(l(inp[-4:]), (2,1), (12, mod.Peak([1.2, 3.4], 1123))), runParser(p.peakline(2), inp))
 #        self.assertEqual(me.MaybeError.zero, runParser(p.dims(2), inp1[:20]))
 
     def testPeakLine3d(self):
         inp = '4436   9.559 129.430 175.123 1 T          1.077e+02  0.00e+00 a   0    0    0    0 0\nhi'
-        inp1 = np.addLineCol(inp)
-        self.assertEqual(good(l(inp1[-2:]), None, (4436, mod.Peak([9.559, 129.430, 175.123], 107.7))), runParser(p.peakline(3), inp1))
+        self.assertEqual(good(l(inp[-2:]), (2,1), (4436, mod.Peak([9.559, 129.430, 175.123], 107.7))), runParser(p.peakline(3), inp))
 
     def testXEeasy(self):
         inp = '''
@@ -54,6 +49,5 @@ class TestXEasy(u.TestCase):
  3 6.243 1 T          0.000e+02  0.00e+00 a   0    0    0    0 0
  4 7.781 1 T          0.000e+02  0.00e+00 a   0    0    0    0 0
 '''
-        inp1 = np.addLineCol(inp[1:])
-        self.assertEqual(good(l([]), None, mod.PeakFile(['H'], {1: mod.Peak([8.795], 0), 2: mod.Peak([7.229], 0),
-                                                                3: mod.Peak([6.243], 0), 4: mod.Peak([7.781], 0)})), runParser(p.xeasy, inp1))
+        self.assertEqual(good(l([]), (7, 1), mod.PeakFile(['H'], {1: mod.Peak([8.795], 0), 2: mod.Peak([7.229], 0),
+                                                                  3: mod.Peak([6.243], 0), 4: mod.Peak([7.781], 0)})), runParser(p.xeasy, inp[1:]))
